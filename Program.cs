40fa1a90d21ab6839bd83bdb539ea42319707405
LautiHumilde <132624519+LautiHumilde@users.noreply.github.com>
2023-10-24 10:11:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        reverse(array);
+
+        string parentesis = "())(";
+
+        if (balanceado(parentesis))
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false");
+        }
+    }
+
+    static void reverse(int[] arr)
+    {
+        for(int i = arr.Length-1 ; i >= 0; i-- ){
+            Console.WriteLine(arr[i]);
+        }
+    }
+
+    static bool balanceado(string parentesis)
+    {
+        int contador = 0;
+
+        foreach (char simbolo in parentesis)
+        {
+            if (simbolo == '(')
+            {
+                contador++;
+            }
+            else if (simbolo == ')')
+            {
+                contador--;
+
+                if (contador < 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return contador == 0;
+    }
+
+}
